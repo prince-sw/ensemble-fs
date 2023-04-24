444976acce57ebf08945b533a31d5e28cce36b33
psw <pswargiary101@gmail.com>
2023-04-25 01:46:27 +0530

    bug fix

--- datasets/config.py
+++ datasets/config.py
@@ -44,112 +44,112 @@ data_files = [
         "no_scale":['a0'],
         "is_multiclass":True
     },
-    {
-        "name": "iono",
-        "drop_columns": [],
-        "encode_columns": ['a34'],
-        "target": "a34",
-        "sep": ",",
-        "no_scale":['a0', 'a1', 'a34'],
-        "is_multiclass":False
-    },
-    {
-        "name": "krkp",
-        "drop_columns": [],
-        "encode_columns": ['a36'],
-        "target": "a36",
-        "sep": ",",
-        "no_scale":['a0', 'a36', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10''a11', 'a12', 'a13', 'a14', 'a15', 'a16', 'a17', 'a18', 'a19', 'a20', 'a21', 'a22', 'a23', 'a24', 'a25', 'a26', 'a27', 'a28', 'a29', 'a30', 'a31', 'a32', 'a33', 'a34', 'a35'],
-        "is_multiclass":False
-    },
-    {
-        "name": "iris",
-        "drop_columns": ["Id"],
-        "encode_columns": ["Species"],
-        "target": "Species",
-        "sep": ",",
-        "no_scale": ["Species"],
-        "is_multiclass": True
-    },
-    {
-        "name": "parkinsons",
-        "drop_columns": [],
-        "encode_columns": ["name", 'status'],
-        "target": "status",
-        "sep": ",",
-        "no_scale":["status"],
-        "is_multiclass":False
-    },
-    {
-        "name": "cirrhosis",
-        "drop_columns": ["ID"],
-        "encode_columns": ["Drug", "Status", "Sex", "Ascites", "Hepatomegaly", "Spiders", "Edema", "Stage"],
-        "target": "Stage",
-        "sep": ",",
-        "no_scale":["Stage", "Drug", "Status", "Sex", "Ascites", "Hepatomegaly", "Spiders", "Edema"],
-        "is_multiclass":True
-    },
-    {
-        "name": "diabetes",
-        "drop_columns": [],
-        "encode_columns": ['Outcome'],
-        "target": "Outcome",
-        "sep": ",",
-        "no_scale":["Outcome", "Age", "BMI", "Pregnancies", "Glucose", "BloodPressure", "SkinThickness", "Insulin", "DiabetesPedigreeFunction"],
-        "is_multiclass":False
-    },
-    {
-        "name": "heart_disease",
-        "drop_columns": [],
-        "encode_columns": ['condition'],
-        "target": "condition",
-        "sep": ",",
-        "no_scale":["condition", "sex", "cp", "fbs", "restecg", "exang", "slope", "ca", "thal"],
-        "is_multiclass":False
-    },
-    {
-        "name": "sonar",
-        "drop_columns": [],
-        "encode_columns": ["Class"],
-        "target": "Class",
-        "sep": ",",
-        "no_scale":["Class"],
-        "is_multiclass":False
-    },
-    {
-        "name": "stroke",
-        "drop_columns": ["id"],
-        "encode_columns": ["gender", 'stroke', "ever_married", "work_type", "Residence_type", "smoking_status"],
-        "target": "stroke",
-        "sep": ",",
-        "no_scale":["stroke", "gender", "ever_married", "work_type", "Residence_type", "smoking_status"],
-        "is_multiclass":False
-    },
-    {
-        "name": "abalone",
-        "drop_columns": [],
-        "encode_columns": ['Age Class'],
-        "target": "Age Class",
-        "sep": ",",
-        "no_scale":["Age Class"],
-        "is_multiclass":True
-    },
-    {
-        "name": "wine",
-        "drop_columns": [],
-        "encode_columns": ['quality'],
-        "target": "quality",
-        "sep": ",",
-        "no_scale": ["quality"],
-        "is_multiclass": False
-    },
-    {
-        "name": "breast",
-        "drop_columns": ["id", "Unnamed: 32"],
-        "encode_columns": ["diagnosis"],
-        "target": "diagnosis",
-        "sep": ",",
-        "no_scale":["diagnosis"],
-        "is_multiclass":False
-    }
+    # {
+    #     "name": "iono",
+    #     "drop_columns": [],
+    #     "encode_columns": ['a34'],
+    #     "target": "a34",
+    #     "sep": ",",
+    #     "no_scale":['a0', 'a1', 'a34'],
+    #     "is_multiclass":False
+    # },
+    # {
+    #     "name": "krkp",
+    #     "drop_columns": [],
+    #     "encode_columns": ['a36'],
+    #     "target": "a36",
+    #     "sep": ",",
+    #     "no_scale":['a0', 'a36', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10''a11', 'a12', 'a13', 'a14', 'a15', 'a16', 'a17', 'a18', 'a19', 'a20', 'a21', 'a22', 'a23', 'a24', 'a25', 'a26', 'a27', 'a28', 'a29', 'a30', 'a31', 'a32', 'a33', 'a34', 'a35'],
+    #     "is_multiclass":False
+    # },
+    # {
+    #     "name": "iris",
+    #     "drop_columns": ["Id"],
+    #     "encode_columns": ["Species"],
+    #     "target": "Species",
+    #     "sep": ",",
+    #     "no_scale": ["Species"],
+    #     "is_multiclass": True
+    # },
+    # {
+    #     "name": "parkinsons",
+    #     "drop_columns": [],
+    #     "encode_columns": ["name", 'status'],
+    #     "target": "status",
+    #     "sep": ",",
+    #     "no_scale":["status"],
+    #     "is_multiclass":False
+    # },
+    # {
+    #     "name": "cirrhosis",
+    #     "drop_columns": ["ID"],
+    #     "encode_columns": ["Drug", "Status", "Sex", "Ascites", "Hepatomegaly", "Spiders", "Edema", "Stage"],
+    #     "target": "Stage",
+    #     "sep": ",",
+    #     "no_scale":["Stage", "Drug", "Status", "Sex", "Ascites", "Hepatomegaly", "Spiders", "Edema"],
+    #     "is_multiclass":True
+    # },
+    # {
+    #     "name": "diabetes",
+    #     "drop_columns": [],
+    #     "encode_columns": ['Outcome'],
+    #     "target": "Outcome",
+    #     "sep": ",",
+    #     "no_scale":["Outcome", "Age", "BMI", "Pregnancies", "Glucose", "BloodPressure", "SkinThickness", "Insulin", "DiabetesPedigreeFunction"],
+    #     "is_multiclass":False
+    # },
+    # {
+    #     "name": "heart_disease",
+    #     "drop_columns": [],
+    #     "encode_columns": ['condition'],
+    #     "target": "condition",
+    #     "sep": ",",
+    #     "no_scale":["condition", "sex", "cp", "fbs", "restecg", "exang", "slope", "ca", "thal"],
+    #     "is_multiclass":False
+    # },
+    # {
+    #     "name": "sonar",
+    #     "drop_columns": [],
+    #     "encode_columns": ["Class"],
+    #     "target": "Class",
+    #     "sep": ",",
+    #     "no_scale":["Class"],
+    #     "is_multiclass":False
+    # },
+    # {
+    #     "name": "stroke",
+    #     "drop_columns": ["id"],
+    #     "encode_columns": ["gender", 'stroke', "ever_married", "work_type", "Residence_type", "smoking_status"],
+    #     "target": "stroke",
+    #     "sep": ",",
+    #     "no_scale":["stroke", "gender", "ever_married", "work_type", "Residence_type", "smoking_status"],
+    #     "is_multiclass":False
+    # },
+    # {
+    #     "name": "abalone",
+    #     "drop_columns": [],
+    #     "encode_columns": ['Age Class'],
+    #     "target": "Age Class",
+    #     "sep": ",",
+    #     "no_scale":["Age Class"],
+    #     "is_multiclass":True
+    # },
+    # {
+    #     "name": "wine",
+    #     "drop_columns": [],
+    #     "encode_columns": ['quality'],
+    #     "target": "quality",
+    #     "sep": ",",
+    #     "no_scale": ["quality"],
+    #     "is_multiclass": False
+    # },
+    # {
+    #     "name": "breast",
+    #     "drop_columns": ["id", "Unnamed: 32"],
+    #     "encode_columns": ["diagnosis"],
+    #     "target": "diagnosis",
+    #     "sep": ",",
+    #     "no_scale":["diagnosis"],
+    #     "is_multiclass":False
+    # }
 ]
